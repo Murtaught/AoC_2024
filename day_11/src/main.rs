cc7cc05cb106ee13a9f1130fe0e2747bb725a17c
Ivan Mikhnovich <murtaught@gmail.com>
2024-12-11 13:04:46 +0300

    D11 P2

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
     let initial_state: State = input
@@ -5,31 +7,51 @@ fn main() {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    println!("ans (p1): {}", simulate(initial_state, 25).len());
+    let mut cache = Cache::new();
+    println!(
+        "ans (p1): {}",
+        count_recursive(&initial_state, 25, &mut cache)
+    );
+    println!(
+        "ans (p2): {}",
+        count_recursive(&initial_state, 75, &mut cache)
+    );
 }
 
 pub type State = Vec<u64>;
 
-pub fn simulate(mut state: State, steps: usize) -> State {
-    eprintln!("Initial state: {state:?}");
-    for _step_no in 0..steps {
-        state = simulate_step(&state);
-        // eprintln!("After step #{}: {state:?}", _step_no + 1);
+// (x, steps) => numbers count
+pub type Cache = HashMap<(u64, usize), usize>;
+
+pub fn count_recursive(state: &State, steps: usize, cache: &mut Cache) -> usize {
+    if steps == 0 {
+        return state.len();
     }
+
     state
+        .iter()
+        .map(|&x| {
+            if let Some(&count) = cache.get(&(x, steps)) {
+                count
+            } else {
+                let next_state = simulate_step(&[x]);
+                let count = count_recursive(&next_state, steps - 1, cache);
+                cache.insert((x, steps), count);
+                count
+            }
+        })
+        .sum()
 }
 
-pub fn simulate_step(state: &State) -> State {
+pub fn simulate_step(state: &[u64]) -> State {
     let mut next_state = vec![];
     for &x in state {
         if x == 0 {
             next_state.push(1);
-        }
-        else if let Some((l, r)) = split_digits(x) {
+        } else if let Some((l, r)) = split_digits(x) {
             next_state.push(l);
             next_state.push(r);
-        }
-        else {
+        } else {
             next_state.push(x * 2024);
         }
     }
