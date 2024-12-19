5d5f5c023f12475d3b76e88bdece4836f0048727
Ivan Mikhnovich <murtaught@gmail.com>
2024-12-19 14:34:44 +0300

    D19 P2

--- day_19/main.py
+++ day_19/main.py
@@ -12,6 +12,17 @@ def can_construct(design: str) -> bool:
 
     return False
 
+@functools.cache
+def count_ways(design: str) -> int:
+    if design == '':
+        return 1
+
+    total_ways = 0
+    for pattern in PATTERNS:
+        if design.startswith(pattern):
+            total_ways += count_ways(design[len(pattern):])
+
+    return total_ways
 
 with open("input") as file:
     lines = [line.strip() for line in file]
@@ -20,11 +31,14 @@ with open("input") as file:
 
     
 ans_p1 = 0
+ans_p2 = 0
+
 n = len(designs)
 for i, design in enumerate(designs):
     # print(f"{i} / {n} ({i * 100.0 / n}%)")
-    if can_construct(design):
-        ans_p1 += 1
+    ans_p1 += int(can_construct(design))
+    ans_p2 += count_ways(design)
 
 print("ans (p1):", ans_p1)
+print("ans (p2):", ans_p2)
 
